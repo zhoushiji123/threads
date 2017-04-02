e9e332f4391545f661a5d851005cf8a2d06fe3eb
81700917@qq.com <81700917@qq.com>
2017-04-02 22:26:06 +0800

    Initial commit

---
+++ src/main/java/com/zsj/ThreadsApplication.java
@@ -0,0 +1,12 @@
+package com.zsj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThreadsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThreadsApplication.class, args);
+	}
+}
---
+++ src/test/java/com/zsj/ThreadsApplicationTests.java
@@ -0,0 +1,16 @@
+package com.zsj;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ThreadsApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
