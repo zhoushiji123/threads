93305979e0c2ef3ef0dadf7530a77d6ad7cba8c4
zhousj <81700917@qq.com>
2017-07-23 20:38:38 +0800

    threadFactory和threadpool

--- src/main/java/com/zsj/threadPool/PoolTest.java
+++ src/main/java/com/zsj/threadPool/PoolTest.java
@@ -11,14 +11,18 @@ public class PoolTest {
 
     public static void main(String[] args) {
 
+        //只以单个工作线程执行的线程池，保证任务按顺序执行
         ExecutorService singleThreadExecutor = Executors.newSingleThreadExecutor();
 
         ExecutorService singleThreadScheduledExecutor = Executors.newSingleThreadScheduledExecutor();
 
+        //可缓存线程池，可灵活回收空闲的线程
         ExecutorService cachedThreadPool = Executors.newCachedThreadPool();
 
+        //定长线程池，控制线程并发数，超出数量的线程会在队列中等待
         ExecutorService  fixedThreadPool = Executors.newFixedThreadPool(10);
 
+        //周期定长线程池，可定时和周期性的执行线程任务
         ExecutorService scheduledThreadPool= Executors.newScheduledThreadPool(10);
 
 
